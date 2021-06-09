ead0fe0f6c9e8f5ac51fef332331fb25b94885b4
Vyacheslav Kuznetsov <VyacheslavKuznetsov@yandex.ru>
2021-06-09 13:24:00 +0300

    Задание №3

--- src/components/Ex2/Blog.js
+++ src/components/Ex2/Blog.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React from "react";
 
 export default class Blog extends React.PureComponent {
-    render() {
-        return (
-            <div></div>
-        )
-    }
+  render() {
+    const data = [
+      {
+        title: "Коты любят мышей!",
+        desc: "Коты, оказывается любят мышей!!!!"
+      },
+      {
+        title: "Кошки боятся собак",
+        desc: "Британские ученые установили, что кошки убегают от собак"
+      },
+      {
+        title: "Собаки боятся кошек",
+        desc: "Другими британскими учеными опровергнуто, что кошки боятся собак. По новым исследованиям собаки боятся кошек."
+      }
+    ];
+
+    const newsCard = ({ title, desc }) => {
+      return (
+        <div
+          style={{
+            boxSizing: "border-box",
+            padding: "10px",
+            border: "1px solid black",
+            width: "30%",
+            borderRadius: "10px",
+          }}
+        >
+          <h2>{title}</h2>
+          <p>{desc}</p>
+        </div>
+      );
+    };
+    return (
+      <div className="Blog">
+        <img
+          style={{
+            maxWidth: "10%",
+          }}
+          src="https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg"
+          alt="Котэ"
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          {data.map((props) => newsCard(props))}
+        </div>
+      </div>
+    );
+  }
 }
